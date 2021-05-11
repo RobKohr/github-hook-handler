ccf9f5ee75d8d8c49fc27b3e107a999d1245353f
Robert Kohr <spam@robkohr.com>
2021-05-11 21:42:51 +0000

    add better documentation

--- example.config.js
+++ example.config.js
@@ -1,8 +1,12 @@
 module.exports ={
     postPath: 'someMadeupRandomString', 
     'repo-name-on-github:main': {
-	cmd:'cd /project-directory; git pull; npm install; npm run build',
+	cmd:'cd /path/to/project-directory; git pull; npm install; npm run build',
 	notes:'Install and build the newest update'
+    },
+    'some-other-repo-name-on-github:weird-branch-name': {
+	cmd:'cd /path/to/project-directory2; git pull; npm install; npm run build',
+	notes:'Put whatevery you want to say about this here'
     },
     port: 3187
 }
