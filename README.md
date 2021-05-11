f35886028de3fc85dbd8e750f451f5935bc668f3
Robert Kohr <spam@robkohr.com>
2021-05-11 21:35:24 +0000

    tested and working

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # github-hook-handler
+
+This will run on the same server as your production repo. When a push happens on github, this
+will run terminal commands, with the goal of updating your repo to the latest from github. 
+
+
+* `npm install`
+* `cp example.config.js local.config.js`
+* Setup your git repo somewhere else on the server
+* update the values in local.config.js
+* Go to https://github.com/your/repo-url and click settings -> webhooks -> add webhook
+* Set the payload url to be http://github-hook-handler-server-url.com:port/postPathFromConfig
+* Just the push event, and then add webook
+* `node server.js` (or use forever or nodemon to keep it up in case of crash)
+* Test by pushing up a commit to the main branch
+* See the log at http://github-hook-handler-server-url.com:port
--- server.js
+++ server.js
@@ -1,12 +1,62 @@
 const express = require('express')
 const config = require('./local.config');
+const bodyParser = require('body-parser');
+const { exec } = require("child_process");
 const app = express()
-const port = 3000
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const logRecord = [`${(new Date()).toString()} server started`];
+const log = (message, res)=>{
+    console.log(message);
+    logRecord.push(message);
+    if(res){
+	res.send(message);
+    }
+}
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(`<html><body><pre>${logRecord.join('\n')}</pre></body></html>`)
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+
+
+app.post(`/${config.postPath}`, (req, res) => {
+    
+    try{
+	console.log(JSON.stringify(JSON.parse(req.body.payload), 0, 5));
+	const payload = JSON.parse(req.body.payload);
+	const messages = payload.commits.map(({message})=>message).join('; ');
+	const name = payload.repository.name;
+	const branch = payload.ref.replace('refs/heads/', '')
+	
+	const cmd = config[name+':'+branch]?.cmd;
+	if(cmd){
+	    log(`${(new Date()).toString()} - deploying repo update for ${name} branch ${branch} - messages: ${messages}`);
+	}else{
+	    return log(`${(new Date()).toString()} - ERROR: command not found for ${name} branch ${branch} - messages: ${messages}`, res);
+	}
+	exec(cmd, (error, stdout, stderr) => {
+	    if (error) {
+		log(`error: ${error.message}`, res);
+		res.sendStatus(500);
+		return;
+	    }
+	    if (stderr) {
+		log(`stderr: ${stderr}`, res);
+		res.sendStatus(500);
+		return;
+	    }
+	    log(`stdout: ${stdout}`, res);
+	    return res.sendStatus(200);
+	});
+
+
+    }catch(e){
+	log(e.toString());
+	res.sendStatus(500);
+    }
+})
+
+app.listen(config.port, () => {
+  console.log(`Example app listening at http://localhost:${config.port}`)
 })
