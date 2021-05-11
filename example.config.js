16c39b38fc2e70de5ff841584d6ba0e25a4dc8cb
Robert Kohr <spam@robkohr.com>
2021-05-11 21:37:26 +0000

    tested and working

---
+++ example.config.js
@@ -0,0 +1,8 @@
+module.exports ={
+    postPath: 'someMadeupRandomString', 
+    'repo-name-on-github:main': {
+	cmd:'cd /project-directory; git pull; npm install; npm run build',
+	notes:'Install and build the newest update'
+    },
+    port: 3187
+}
